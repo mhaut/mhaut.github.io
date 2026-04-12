dc522ca3f9aa0980a727661f0532796883ecc700
mhaut <mhaut@users.noreply.github.com>
2026-04-12 10:59:22 +0000

    Deploying to gh-pages from @ mhaut/mhaut.github.io@03010b8a7253b682b22ddac5db4b97177acbc4a6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -58,13 +58,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/students/";
           },
-        },{id: "nav-cv",
-          title: "CV",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/cv/";
-          },
         },{id: "nav-teaching",
           title: "Teaching",
           description: "Courses taught at the University of Extremadura and UNED.",
